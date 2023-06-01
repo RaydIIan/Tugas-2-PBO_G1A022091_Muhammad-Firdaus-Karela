0a0e83dd9e5545900f47da57743f7af7aa506f4e
Muhammad Firdaus Karela <131359860+RaydIIan@users.noreply.github.com>
2023-06-01 22:47:39 +0700

    Implementasi Kelas Mahasiswa Jurusan dan Universitas.py

--- Implementasi Kelas Mahasiswa Jurusan dan Universitas.py
+++ Implementasi Kelas Mahasiswa Jurusan dan Universitas.py
@@ -7,40 +7,38 @@ class Mahasiswa:
         self.jurusan = jurusan
 
     def tampilkan_info(self):
-        print("Nama: ", self.nama)
-        print("NIM: ", self.nim)
-        print("Jurusan: ", self.jurusan.NamaJurusan)
+        print("Nama:", self.nama)
+        print("NIM:", self.nim)
+        print("Jurusan:", self.jurusan.nama_jurusan)
 
 
 class Jurusan:
     def __init__(self, nama_jurusan):
-        self.NamaJurusan = nama_jurusan
-        self.DaftarMahasiswa = []
+        self.nama_jurusan = nama_jurusan
+        self.daftar_mahasiswa = []
 
     def tambah_mahasiswa(self, mahasiswa):
-        self.DaftarMahasiswa.append(mahasiswa)
+        self.daftar_mahasiswa.append(mahasiswa)
 
     def tampilkan_daftar_mahasiswa(self):
-        print("Daftar Mahasiswa di Jurusan", self.NamaJurusan)
-        for mahasiswa in self.DaftarMahasiswa:
-            print("Nama: ", mahasiswa.nama)
-            print("NIM: ", mahasiswa.nim)
+        print("Daftar Mahasiswa di Jurusan", self.nama_jurusan)
+        for mahasiswa in self.daftar_mahasiswa:
+            mahasiswa.tampilkan_info()
             print()
 
 
 class Universitas:
     def __init__(self, nama_universitas):
-        self.NamaUniversitas = nama_universitas
-        self.DaftarJurusan = []
+        self.nama_universitas = nama_universitas
+        self.daftar_jurusan = []
 
     def tambah_jurusan(self, jurusan):
-        self.DaftarJurusan.append(jurusan)
+        self.daftar_jurusan.append(jurusan)
 
     def tampilkan_daftar_jurusan(self):
-        print("Daftar Jurusan di Universitas", self.NamaUniversitas)
-        for jurusan in self.DaftarJurusan:
-            print("Nama Jurusan: ", jurusan.NamaJurusan)
-            print()
+        print("Daftar Jurusan di Universitas", self.nama_universitas)
+        for jurusan in self.daftar_jurusan:
+            print("Nama Jurusan:", jurusan.nama_jurusan)
 
 
 # Contoh penggunaan program
